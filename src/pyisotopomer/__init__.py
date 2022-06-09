885e31b9fe6497b79b4438c6dcc18132a4a77e07
ckelly314 <clkelly@stanford.edu>
2022-06-09 13:44:08 -0700

    add IsotopomerInput to __init__

--- src/pyisotopomer/__init__.py
+++ src/pyisotopomer/__init__.py
@@ -1,5 +1,6 @@
 # pyisotopomer: Nitrous oxide isotopocule data corrections in Python
 # Copyright (C) 2021  Colette L Kelly et al.  (MIT License)
 
+from .isotopomerinput import IsotopomerInput
 from .pyisotopomer import Scrambling
 from .pyisotopomer import Isotopomers
